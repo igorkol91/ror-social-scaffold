dda1df1643e0d389a2dd25a5889b9c40adb62be8
Igor.Koloski <koloski_igor@hotmail.com>
2021-11-02 14:31:04 +0100

    Add comments endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :posts, only: [:index, :create]
+      resources :posts, only: [:index, :create] do
+        resources :comments, only: [:index, :create]
+      end
     end 
   end
 
