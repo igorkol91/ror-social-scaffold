f79b1d9c4c0a84924de29494ee7044c5eb749242
Igor.Koloski <koloski_igor@hotmail.com>
2021-11-02 15:08:37 +0100

    Add jwt auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      post :auth, to: "authentication#create"
       resources :posts, only: [:index, :create] do
         resources :comments, only: [:index, :create]
       end
